4c605d51fabea49bfa481eea1eabc066d6eb6064
Michael Peterson <michaeljoepeterson@gmail.com>
2019-07-13 17:49:36 -0600

    add initial slideshow class and css

--- src/components/index.js
+++ src/components/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Route, withRouter} from 'react-router-dom';
 import {getBills} from "../actions/allBillActions";
+import {Slideshow} from './slideshow';
 import Loader from './loader';
 
 export class App extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.slideId = "test-slideshow";
+	}
 	componentDidMount(){
 	    this.props.dispatch(getBills(10));
-	 }
+	}
+	//will need to do something like this to render buttons outside of box and still interact with box
+	buttonClicked(event){
+		let slideshow = document.getElementById(this.slideId);
+		console.log(slideshow);
+	}
 	render(){
 		let loader;
 		console.log(this.props.billData.loading);
@@ -18,6 +29,8 @@ export class App extends React.Component{
 			<div>
 				{loader}
 				{this.props.billData.bills.length}
+				<Slideshow slideId={this.slideId}/>
+				<button onClick={(e)=>this.buttonClicked(e)}>test</button>
 			</div>
 		);
 		
---
+++ src/components/slideshow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/slideshow.css";
+
+export class Slideshow extends React.Component{
+	render(){
+		return(
+			<div className="slide-container" id={this.props.slideId}>
+
+			</div>
+		)
+	}
+}
