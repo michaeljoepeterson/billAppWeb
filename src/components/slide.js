05b195af472749a9a427747731467833d23ace23
Michael Peterson <michaeljoepeterson@gmail.com>
2019-07-14 10:42:27 -0600

    added some slide styles

--- src/components/slide.js
+++ src/components/slide.js
@@ -3,7 +3,7 @@ import React from 'react';
 export class Slide extends React.Component{
 
 	render(){
-		console.log("inside slides ", this.props.billData);
+		console.log("inside slides ", this.props.bill);
 		return(
 			<div className="slide">
 				<p>test</p>
