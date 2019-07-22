8bf88076b3c0f33371ece602ddbbf508c7ecee4e
Michael Peterson <michaeljoepeterson@gmail.com>
2019-07-22 16:47:14 -0600

    updated slides to render all bill data obtained from api call

--- src/components/slides.js
+++ src/components/slides.js
@@ -4,13 +4,16 @@ import {Slide} from './slide';
 export class Slides extends React.Component{
 	
 	renderSlides(bills){
-		/*
-		for(let i = 0;i < bills.length;i++){
-
-		}
-		*/
 		if(bills.length !== 0){
-			let slideComponents = [<Slide key="0" bill={bills[0]} firstSlide={true} slideWidth={this.props.slideWidth}/>,<Slide key="1" bill={bills[1]} slideWidth={this.props.slideWidth}/>];
+			let slideComponents = [];
+			for(let i = 0;i < bills.length;i++){
+				let isFirstSlide = false;
+				if(i === 0){
+					isFirstSlide = true;
+				}
+				slideComponents.push(<Slide key={i} bill={bills[i]} slideWidth={this.props.slideWidth} firstSlide={isFirstSlide}/>)
+			}
+
 			return slideComponents;
 		}
 	}
